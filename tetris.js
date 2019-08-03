5cef07a8fc9fe6b3d2e865d2e9d6b44bd6975156
Suvansh Shukla <suvanshshukla@gmail.com>
2019-08-03 18:24:29 +0530

    capture pieces and save them into extra space (arena)

--- tetris.js
+++ tetris.js
@@ -11,6 +11,14 @@ const matrix = [    //->this is the data structure for holding the tetriminos, a
     [0, 1, 0]
 ];
 
+function createMatrix(w, h) {   //## this function is made to draw all tetriminos
+    const matrix = [];  //declare an empty matrix 
+    while (h--){
+        matrix.push(new  Array(w).fill(0)); //push zeroes into it
+    }
+    return matrix;
+}
+
 function draw() {   //OKK made a separate function to draw tetriminos
     context.fillStyle = '#000'; //-> this and the line below it are written to refresh the canvas 
     context.fillRect(0, 0, canvas.width, canvas.height);
@@ -32,6 +40,21 @@ function drawMatrix(matrix, offset) {
     });
 }
 
+function merge(arena, player) {     //>> this function will copy the values of the player into the arena at the correct positions 
+    player.matrix.forEach((row, y) =>{
+        row.forEach((value, x) =>{
+            if (value !== 0){
+                arena[y + player.pos.y][x + player.pos.x] = value;
+            }
+        })
+    })
+}
+
+function playerDrop() {
+    player.pos.y++; //>> here after every second the ordinate value of the tetrimino is incremented 
+    dropCounter = 0;    //>> here the drop counter is reset after every drop inorder to keep the drops uniform
+}
+
 let dropCounter = 0;    //->we set a drop counter to count how much time later the next drop in ordinate should occur
 let dropInterval = 1000;    //->this is the interval with which we compare to drop the tetrimino
 
@@ -42,13 +65,15 @@ function update(time = 0) {  //>> this was made to draw the game continously
 
     dropCounter += deltaTime;
     if(dropCounter > dropInterval) {
-        player.pos.y++; //>> here after every second the ordinate value of the tetrimino is incremented 
-        dropCounter = 0;    //>> here the drop counter is reset after every drop inorder to keep the drops uniform
+        playerDrop();
     }
     
     draw();
     requestAnimationFrame(update);
 }
+
+const arena = createMatrix(12, 20); //>> this is made for capturing uncleared/stuck pieces
+
 
 const player = {
     pos: { x: 5, y: 5 },
@@ -62,6 +87,9 @@ document.addEventListener('keydown', event=>{
     }
     else if(event.keyCode === 39){
         player.pos.x++;
+    }
+    else if(event.keyCode === 40){
+        playerDrop();
     }
     
     
